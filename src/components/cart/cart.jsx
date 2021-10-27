920036715279999638db23c9a16ba718b21fea43
mariagluck <mariamart.berlin@gmail.com>
2021-10-28 00:08:38 +0200

    continue developing

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { withRouter } from 'react-router-dom';
+import { CartContext } from '../../context/cart-context';
 import shoppingBag from '../../images/shopping_bag.png';
 import './cart_styles.scss';
 
 const CartIcon = ({ history }) => {
+  const { itemCount } = useContext(CartContext);
+
   return (
     <div className="cart-container" onClick={() => history.push('/cart')}>
       <img src={shoppingBag} alt='shopping-cart-icon' />
-      <span className='cart-count' id='lblCartCount'> 5 </span>      
+      {
+        itemCount > 0 ? <span className='cart-count' id='lblCartCount'> { itemCount } </span> : null
+      }
     </div>
   );
 }
 
-export default CartIcon; 
+export default withRouter(CartIcon); 
--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -8,8 +8,12 @@ const initialState = { cartItems: [], itemCount: 0, total: 0 };
 const CartContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
 
+  const addProduct = payload => {
+    dispatch({type: 'ADD_ITEM', payload});
+  }
   const contextValues = {
     ...state,
+    addProduct,
   }
 
   return (
